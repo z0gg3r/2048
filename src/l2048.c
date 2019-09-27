b03631523aaeac7d6bea5c2845686e3bfed8083c
z0gg3r <z0gg3r@reddev.org>
2019-09-27 13:24:29 +0200

    Now the GRID_X and GRID_Y equate to the actual playing size. A merge function has been implemented. All existing functions no longer need to deal with borders and have been modified accordingly. Started to implement 'move_up'.

--- src/l2048.c
+++ src/l2048.c
@@ -6,12 +6,7 @@ board init(void)
     board b;
     for (int i = 0; i < GRID_Y; i++) {
         for (int j = 0; j < GRID_X; j++) {
-            if (i == 0 || j == 0)
-                b.state[i][j] = '#';
-            else if (j + 1 == GRID_X || i + 1 == GRID_Y)
-                b.state[i][j] = '#';
-            else
-                b.state[i][j] = '\0';
+            b.state[i][j] = '\0';
         }
     }
     return b;
@@ -22,20 +17,20 @@ int calc_points(board b)
     int points = 0;
     for (int i = 0; i < GRID_Y; i++) {
         for (int j = 0; j < GRID_X; j++) {
-            char *c = &b.state[i][j];
-            if (i == 0 || j == 0)
-                points += 0;
-            else if (j + 1 == GRID_X || i + 1 == GRID_Y)
-                points += 0;
-            else if (b.state[i][j] == '\0')
-                points += 0;
-            else
+            if (b.state[i][j] != '\0') {
+                char *c = &b.state[i][j];
                 points += atoi(c);
+            }
         }
     }
     return points;
 }
 
+char merge(char *x, char *y)
+{
+    int merged = atoi(x) + atoi(y);
+    return (char) merged;
+}
 
 /*--------  Movement  --------*/
 /*
@@ -47,6 +42,13 @@ int calc_points(board b)
 
 board move_left(board b)
 {
+    for (int i = 0; i < GRID_Y; i++) {
+        for (int j = 0; j < GRID_X; j++) {
+            if (b.state[i][j] == '\0' && b.state[i][j + 1] != '\0') {
+                
+            }
+        }
+    }
     return b;
 }
 
--- src/l2048.h
+++ src/l2048.h
@@ -1,15 +1,16 @@
 #ifndef L2048_H
 #define L2048_H
 
-#define GRID_X 6
-#define GRID_Y 6
+/*
+/ GRID_X and GRID_Y should be 2 bigger then the actual playing field
+*/
+#define GRID_X 4
+#define GRID_Y 4
 #define END_OF_GAME 2048
 
 typedef struct board {
     char state[GRID_Y][GRID_X];
 } board;
-
-#define MERGE(x) x*2
 
 board init(void);
 board move_left(board);
@@ -17,6 +18,6 @@ board move_right(board);
 board move_up(board);
 board move_down(board);
 int calc_points(board);
-
+char merge(char*, char*);
 
 #endif // !L2048_H
