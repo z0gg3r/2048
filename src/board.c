8a0926a46430a2d5f47d0f3fd471cedd64047b7e
z0gg3r <z0gg3r@reddev.org>
2020-04-11 22:06:19 +0200

    So yeah, the fix did not work so I fixed the fix.

--- src/board.c
+++ src/board.c
@@ -21,7 +21,7 @@ void mv_left(board_t *b)
 			limit = NUM_CELLS - (1 + j);
 			while (idx < limit) {
 				cell = j + 1 + idx;
-				if (b->cols[i]->cells[j] && b->cols[i]->cells[cell] > b->cols[i]->cells[j]) {
+				if (b->cols[i]->cells[j] && b->cols[i]->cells[cell] && b->cols[i]->cells[cell] != b->cols[i]->cells[j]) {
 					// There might be merge candidates down the line but
 					// they are blocked and thus we break.
 					break;
@@ -69,7 +69,7 @@ void mv_right(board_t *b)
 			limit = j;
 			while (idx < limit) {
 				cell = j - 1 - idx;
-				if (b->cols[i]->cells[j] && b->cols[i]->cells[cell] > b->cols[i]->cells[j]) {
+				if (b->cols[i]->cells[j] && b->cols[i]->cells[cell] && b->cols[i]->cells[cell] != b->cols[i]->cells[j]) {
 					// There might be merge candidates down the line, but
 					// they are blocked, thus we break.
 					break;
@@ -117,7 +117,7 @@ void mv_up(board_t *b)
 			limit = NUM_COLUMNS - (1 + j);
 			while (idx < limit) {
 				col = j + 1 + idx;
-				if (b->cols[j]->cells[i] && b->cols[col]->cells[i] > b->cols[j]->cells[i]) {
+				if (b->cols[j]->cells[i] && b->cols[col]->cells[i] && b->cols[col]->cells[i] != b->cols[j]->cells[i]) {
 					// There might be merge candidates down the line,
 					// but they are blocked and thus we break.
 					break;
@@ -165,7 +165,7 @@ void mv_down(board_t *b)
 			limit = j;
 			while (idx < limit) {
 				col = j - 1 - idx;
-				if (b->cols[j]->cells[i] && b->cols[col]->cells[i] > b->cols[j]->cells[i]) {
+				if (b->cols[j]->cells[i] && b->cols[col]->cells[i] && b->cols[col]->cells[i] != b->cols[j]->cells[i]) {
 					// There might be merge candidates down
 					// the line, but they are blocked, thus
 					// we break
