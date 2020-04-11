f300516061ba8c41ec9c87fe246c565b9b1770a8
z0gg3r <z0gg3r@reddev.org>
2020-04-11 21:19:52 +0200

    Fixed the mem leak

--- src/main.c
+++ src/main.c
@@ -12,16 +12,21 @@ int main()
 	board_t b;
 	b.points = 0;
 	for (int i = 0; i < NUM_COLUMNS; ++i) {
-		column_t c;
+		column_t *c = malloc(sizeof(column_t));
 		for (int j = 0; j < NUM_CELLS; ++j) {
-			c.cells[j] = 0;
+			c->cells[j] = 0;
 		}
-		b.cols[i] = &c;
+		b.cols[i] = c;
 	}
+	draw(&b);
 	spawn(&b, 1);
 	draw(&b);
 	mv_left(&b);
 	draw(&b);
+	for (int i = 0; i < NUM_COLUMNS; ++i) {
+		free(b.cols[i]);
+	}
+	return 0;
 }
 
 void draw(board_t *b)
