450dd553f60254d301a3121f2bac668792274c23
z0gg3r <z0gg3r@reddev.org>
2020-04-12 20:42:49 +0200

    Improved documentation

--- src/main.c
+++ src/main.c
@@ -20,6 +20,12 @@ int game_loop();
 
 int main()
 {
+	/*
+	 * We initalize the setup of ncurses, seed srand and then enter the
+	 * game loop and loop until the users tells us that they have had
+	 * enough of the game. We then end the ncurses session and quit.
+	 *
+	 */
 	initscr();
 	cbreak();
 	noecho();
@@ -36,6 +42,21 @@ int main()
 
 int game_loop()
 {
+	/*
+	 * We first unset the reset flag (so that we can actually play
+	 * multiple times) and then clear the screen. We create a new
+	 * board and spawn the first to values. We then draw the current
+	 * board and wait for user input to move in that direction, reset
+	 * or quit. If after the switch either reset, quit or game_over
+	 * are true, we exit the while loop, if not we repeat the last
+	 * sentence. When the while loop exits, we print a message with
+	 * the final points, call free_board to deallocate memory and
+	 * then either return reset, quit or wait for user input.
+	 * If we wait for user input, we wait until the user enters
+	 * either `r` to start a new game or `q` to quit. We then return
+	 * what the user entered.
+	 *
+	 */
 	FLAG_RESET = 0;
 	clear();
 	board_t *b = init();
@@ -91,6 +112,14 @@ int game_loop()
 
 void draw(board_t *b)
 {
+	/*
+	 * Currently we first print the number of points
+	 * and then all values. This will change in the
+	 * future so that all cells are in the same spot
+	 * all the time and not three to left if the value
+	 * of a previous cell is greater than one hundred.
+	 *
+	 */
 	printw("Points: %d \n", b->points);
 	for (int i = 0; i < NUM_COLUMNS; ++i) {
 		for (int j = 0; j < NUM_CELLS; ++j) {
@@ -103,6 +132,14 @@ void draw(board_t *b)
 
 int remap(int ch)
 {
+	/*
+	 * This is called every time the user enters an input
+	 * and returns a quick response code for the appropritate
+	 * action so that the code that handles the action can
+	 * look a bit neater!
+	 * (The values for the return codes are defined under
+	 * the header files)
+	 */
 	if (ch == KEY_LEFT || ch == 'h' || ch == 'H') {
 		return LEFT;
 	} else if (ch == KEY_RIGHT || ch == 'l' || ch == 'L') {
