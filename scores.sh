387dc36c0bcbf10c4eb7d2b215122c40413eaff5
zocker <zocker@10zen.eu>
2022-05-13 15:15:09 +0200

    minor change to scores.sh

--- scores.sh
+++ scores.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 
 SCORES="$HOME/.2048"
+TMPFILE=$(mktemp)
 
 if [ -e "$SCORES" ]
 then
 	echo "$1" >> "$SCORES"
-	sort -rn < "$SCORES" | sed 20q > "$SCORES.new"
-	mv "$SCORES.new" "$SCORES"
+	sort -rn < "$SCORES" | sed 20q > "$TMPFILE"
 else
-	echo "$1" > "$SCORES"
+	echo "$1" > "$TMPFILE"
 fi
+
+mv "$TMPFILE" "$SCORES"
